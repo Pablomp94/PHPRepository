e1e750504b4f5703fde0c3e51038ca2550574c9b
Pablomp94 <1744623@alu.murciaeduca.es>
2024-11-04 17:47:29 +0100

    PHP

--- ejercicioBucles/calendarioCompletoV3.php
+++ ejercicioBucles/calendarioCompletoV3.php
@@ -31,59 +31,64 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     </style>
     <body>
         <?php
-        $meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-        $diaMeses = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        $semana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
 
-        $pos = 0;
-        for ($i = 0; $i < sizeof($meses); $i++) {
-            echo "<table>";
-            echo "<tr>";
-            echo "<th>" . $meses[$i] . "</th>";
-            echo "</tr>";
-            $x = 1;
-
-            echo "<tr>";
-            foreach ($semana as $sem) {
-                echo "<th>" . $sem . " " . "</th>";
-            }
-            echo "</tr>";
-            echo "<tr>";
-
-            $blanco = 0;
-            $atras = $pos;
-
-            if($meses[$i] != "Enero"){
-                $ultimoNumero = $diaMeses[$i - 1] - ($pos - 1);
-            }else{
-              $ultimoNumero = null;  
-            }
-            
-            while ($blanco < $pos) {
-                echo "<td class=nuevo>" . $ultimoNumero . "</td>";
-                $ultimoNumero++;
-                $blanco++;
-            }
+        function crearCalendario() {
+            $meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+            $diaMeses = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+            $semana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
 
             $pos = 0;
+            for ($i = 0; $i < sizeof($meses); $i++) {
+                echo "<table>";
+                echo "<tr>";
+                echo "<th>" . $meses[$i] . "</th>";
+                echo "</tr>";
+                $x = 1;
 
-            while ($x <= $diaMeses[$i]) {
-
-                if (($x != 0) && ((($x + $blanco) - 1) % 7 == 0)) {
-                    echo"</tr>";
-                    echo "<tr>";
-                    echo "<td>" . $x . "</td>";
-                    $pos = 0;
-                } else {
-                    echo "<td>" . $x . "</td>";
+                echo "<tr>";
+                foreach ($semana as $sem) {
+                    echo "<th>" . $sem . " " . "</th>";
                 }
-                $x++;
-                $pos++;
+                echo "</tr>";
+                echo "<tr>";
+
+                $blanco = 0;
+                $atras = $pos;
+
+                if ($meses[$i] != "Enero") {
+                    $ultimoNumero = $diaMeses[$i - 1] - ($pos - 1);
+                } else {
+                    $ultimoNumero = null;
+                }
+
+                while ($blanco < $pos) {
+                    echo "<td class=nuevo>" . $ultimoNumero . "</td>";
+                    $ultimoNumero++;
+                    $blanco++;
+                }
+
+                $pos = 0;
+
+                while ($x <= $diaMeses[$i]) {
+
+                    if (($x != 0) && ((($x + $blanco) - 1) % 7 == 0)) {
+                        echo"</tr>";
+                        echo "<tr>";
+                        echo "<td>" . $x . "</td>";
+                        $pos = 0;
+                    } else {
+                        echo "<td>" . $x . "</td>";
+                    }
+                    $x++;
+                    $pos++;
+                }
+
+
+                echo "</table>";
             }
-
-
-            echo "</table>";
         }
+
+        crearCalendario();
         ?>
     </body>
 </html>
