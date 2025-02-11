430e3670f41a2a16ded2416b65ad8e9753ac68e5
Pablomp94 <1744623@alu.murciaeduca.es>
2025-02-11 12:19:06 +0100

    PHP

--- baseDatos/primeraPrueba.php
+++ baseDatos/primeraPrueba.php
@@ -13,11 +13,12 @@
     if($conn->connect_error){
         die("La conexion ha fallado: " . $conn->connect_error);
     }else{
-        echo "La conexion se ha realizado correctamente";
+        echo "La conexion se ha realizado correctamente </br>";
     }
     
     
     //A continuacion almacenamos en una variable la consulta sql que queremos ejecutar
+    //CREAMOS LA TABLA
     $sql = "CREATE TABLE MyGuests (
             id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             firstname VARCHAR(30) NOT NULL,
@@ -26,9 +27,60 @@
             reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             )";
     
+    //INSERTAMOS DATOS EN LA TABLA
+    $sql = "INSERT INTO myguests (firstname, lastname, email) 
+            VALUES('Pablo', 'asdd','asd@gmail.com') ";
     
     
-
+    $sql = "INSERT INTO myguests(firstname,lastname,email)
+            VALUES('Pepe', 'asddddsd', 'fasjdsa@gmail.com')";
+    
+    
+    $sql = "SELECT id, firstname, lastname, reg_date FROM myguests";
+    
+    $result = mysqli_query($conn, $sql);
+    
+    $idArray = [];
+    $idAñadir = [];
+    
+    if(mysqli_num_rows($result) > 0){
+        
+        while($row = mysqli_fetch_assoc($result)){
+            array_push($idArray, $row["id"]);
+            
+            echo "Id: " . $row["id"] . ". Nombre: " . $row["firstname"] . ". Apellidos: " . $row["firstname"] . $row["lastname"] . ". Fecha: " . $row["reg_date"] . "</br>";
+        }
+        
+        $tam = sizeof($idArray);
+        
+        $x = 1;
+        
+        for($i = 0; $i < $tam; $i++){
+            $num = ($i + $x);
+            if($idArray[$i] != $num){
+                echo ($num);
+                $x++;
+                //Array dnd guardo los ids que no existen a añadir
+                array_push($idAñadir, $num);
+            }
+        }
+        
+    }
+    
+    
+    
+    //Ejecutamos la consulta sql
+    if($conn->query($sql) == TRUE){
+        $last_id = mysqli_insert_id($conn); //DEVUELVE EL ID DEL ULTIMO REGISTRO QUE SE INSERTO EN UNA TABLA CON UNA COLUMNA AUTO-INCREMENT
+        echo "La consulta: " . $sql . " se ha iniciado con exito.</br>";
+        echo "El ultimo id insertado es: " . $last_id;
+    }else{
+        echo "Error ejecutando la consulta: " . $conn->error;
+    }
+    
+    
+    //CERRAMOS LA CONEXION
+    mysqli_close($conn);  
 ?>
 
 <html>
